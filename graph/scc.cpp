04498ce1ea20b697288f24b82c3219f081b5032a
Asad <asad.binsaber@gmail.com>
2023-04-07 22:23:43 +0600

    updated scc

--- graph/scc.cpp
+++ graph/scc.cpp
@@ -1,54 +1,82 @@
-# Not Tested
+#include <bits/stdc++.h>
+using namespace std;
 
-/// Finding SCC
+vector<vector<int>> adj, adj_rev;
+vector<bool> used;
+vector<int> order, component;
 
-// For directed edges
-// Need a reverse edge graph
-
-const int MX = 1e5;
-vector<int> edge[MX+5], reverse_edge[MX+5], component[MX+5];
-bool vis[MX+5], color[MX+5];
-stack<int> stk;
-
-void dfs(int u)
-{
-    color[u] = 1;
+void dfs1(int u) {
+    used[u] = true;
     
-    for(int K = 0; K < (int)edge[K].size(); K++){
-        if(color[edge[u][K]] == 0) dfs(edge[u][K]);
-    }
-    stk.push(u);
+    for (auto v : adj[u])
+        if (!used[v])
+            dfs1(v);
+
+    order.push_back(u);
 }
 
-void dfs2(int u, int mark)
-{
-    component[mark].push_back(u);
-    vis[u] = 1;
-    
-    for(int K = 0; K < (int)reverse_edge[u].size(); K++){
-        if(!vis[reverse_edge[u][K]]) dfs2(reverse_edge[u][K], mark);
-    }
+void dfs2(int u) {
+    used[u] = true;
+    component.push_back(u);
+
+    for (auto v : adj_rev[u])
+        if (!used[v])
+            dfs2(v);
 }
 
-int find_scc(int n)
-{
-    int mark = 0;
-    
-    for(int K = 1; K <= n; K++){
-        if(color[K] == 0) dfs(K);
-    }
-    
-    while(!stk.empty()){
-        int u = stk.top();
-        stk.pop();
+int main() {
+    int n;
+    // ... read n ...
+
+    for (;;) {
+        int a, b; cin >> a >> b;
         
-        if(vis[u] == 0){
-            mark++;
-            dfs2(u, mark);
-        }
+        adj[a].push_back(b);
+        adj_rev[b].push_back(a);
     }
-    
-    // We get components as result
-    return mark; // Number for SCC subgraph.
+
+    used.assign(n, false);
+
+    for (int i = 0; i < n; i++)
+        if (!used[i])
+            dfs1(i);
+
+    used.assign(n, false);
+    reverse(order.begin(), order.end());
+
+    for (auto v : order)
+        if (!used[v]) {
+            dfs2 (v);
+
+            // ... processing next component ...
+
+            component.clear();
+        }
 }
 
+/// Condensation Graph Implementation
+vector<int> roots(n, 0);
+vector<int> root_nodes;
+vector<vector<int>> adj_scc(n);
+
+//for (auto v : order)
+    //if (!used[v]) {
+        //dfs2(v);
+
+        int root = component.front();
+        for (auto u : component) roots[u] = root;
+        root_nodes.push_back(root);
+
+        //component.clear();
+    //}
+
+
+for (int v = 0; v < n; v++){
+    for (auto u : adj[v]) {
+        int root_v = roots[v],
+            root_u = roots[u];
+
+        if (root_u != root_v)
+            adj_scc[root_v].push_back(root_u);
+    }
+}
