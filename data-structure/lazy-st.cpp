d0dd52a93f040c79b786e9e4011f8c170d80dabf
Asad Bin Saber <mdasad.binsaber@gmail.com>
2024-07-11 18:17:05 +0600

    changed lazy code

--- data-structure/lazy-st.cpp
+++ data-structure/lazy-st.cpp
@@ -1,49 +1,61 @@
-ll seg[400005], a[100005], lazy[400005];
-// can lazy be zero by default ,check question
-// if we have multiple test case then check seg,a,lazy array
-// 0 index or 1 index for query and update
+const int N = 5e5 + 9;
+int a[N];
 
-void build(int pos, int l, int r) {
-    lazy[pos] = 0;
-    if (l == r) {
-        seg[pos] = a[l];
-        return;
+struct ST {
+  #define lc (n << 1)
+  #define rc ((n << 1) | 1)
+  long long t[4 * N], lazy[4 * N];
+  ST() {
+    memset(t, 0, sizeof t);
+    memset(lazy, 0, sizeof lazy);
+  }
+  inline void push(int n, int b, int e) {
+    if (lazy[n] == 0) return;
+    t[n] = t[n] + lazy[n] * (e - b + 1);
+    if (b != e) {
+      lazy[lc] = lazy[lc] + lazy[n];
+      lazy[rc] = lazy[rc] + lazy[n];
     }
-    ll mid = (l + r) >> 1;
-    build(2 * pos, l, mid);
-    build(2 * pos + 1, mid + 1, r);
-    seg[pos] = seg[2 * pos] + seg[2 * pos + 1];
-}
-void lazy_update(int pos, int l, int r) {
-    seg[pos] += (r - l + 1) * lazy[pos];
-    if (l != r) {
-        lazy[2 * pos] += lazy[pos];
-        lazy[2 * pos + 1] += lazy[pos];
+    lazy[n] = 0;
+  }
+  inline long long combine(long long a,long long b) {
+    return a + b;
+  }
+  inline void pull(int n) {
+    t[n] = t[lc] + t[rc];
+  }
+  void build(int n, int b, int e) {
+    lazy[n] = 0;
+    if (b == e) {
+      t[n] = a[b];
+      return;
     }
-    lazy[pos] = 0;
-}
-void update(int pos, int l, int r, int x, int y, ll val) {
-    if (lazy[pos] != 0) lazy_update(pos, l, r);
-
-    if (l > y || x > r) return;
-    if (l >= x && r <= y) {
-        lazy[pos] += val;
-        lazy_update(pos, l, r);
-        return;
+    int mid = (b + e) >> 1;
+    build(lc, b, mid);
+    build(rc, mid + 1, e);
+    pull(n);
+  }
+  void upd(int n, int b, int e, int i, int j, long long v) {
+    push(n, b, e);
+    if (j < b || e < i) return;
+    if (i <= b && e <= j) {
+      lazy[n] = v; //set lazy
+      push(n, b, e);
+      return;
     }
-    int mid = (l + r) >> 1;
-    update(2 * pos, l, mid, x, y, val);
-    update(2 * pos + 1, mid + 1, r, x, y, val);
-    seg[pos] = seg[2 * pos] + seg[2 * pos + 1];
+    int mid = (b + e) >> 1;
+    upd(lc, b, mid, i, j, v);
+    upd(rc, mid + 1, e, i, j, v);
+    pull(n);
+  }
+  long long query(int n, int b, int e, int i, int j) {
+    push(n, b, e);
+    if (i > e || b > j) return 0; //return null
+    if (i <= b && e <= j) return t[n];
+    int mid = (b + e) >> 1;
+    return combine(query(lc, b, mid, i, j), query(rc, mid + 1, e, i, j));
+  }
+};
+int32_t main() {
+    
 }
-ll query(int pos, int l, int r, int x, int y) {
-    if (lazy[pos] != 0) lazy_update(pos, l, r);
-
-    if (l > y || x > r) return 0;
-    if (l >= x && r <= y) return seg[pos];
-    int mid = (l + r) >> 1;
-    ll q1 = query(2 * pos, l, mid, x, y);
-    ll q2 = query(2 * pos + 1, mid + 1, r, x, y);
-    return q1 + q2;
-}
-
