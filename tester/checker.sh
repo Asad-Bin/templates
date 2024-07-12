595fbc6844051760612f77eb89c1bc5f2664d215
Asad Bin Saber <mdasad.binsaber@gmail.com>
2024-07-12 12:04:48 +0600

    checker file updated

--- tester/checker.sh
+++ tester/checker.sh
@@ -1,32 +1,27 @@
-# rem @echo off
-# rem g++ -std=c++17 -o test.exe test.cpp
-# rem g++ -std=c++17 -o soln.exe soln.cpp
-# rem g++ -std=c++17 -o brute.exe brute.cpp
-# rem for /l %%x in (1, 1, 100) do (
-# rem     test > input.in
-# rem     soln < input.in > output.out 
-# rem     brute < input.in > output2.out
-# rem     fc output.out output2.out > diagnostics || exit /b
-# rem     echo %%x
-# rem )
-# rem echo all tests passed
-# rem pause
 set -e
 g++ -std=c++17 -O2 -Wshadow -Wall -Wextra -Wshift-overflow=2 -fno-sanitize-recover -fstack-protector -o "%e" -g -D_GLIBCXX_DEBUG a.cpp -o a
-g++ -std=c++17 test.cpp -o test
+g++ -std=c++17 gen.cpp -o gen
 g++ -std=c++17 brute.cpp -o brute
-for((i = 1; i < 1000; ++i)); do
-    ./test $i > input_file
+
+ok=1
+for((i = 1; i < 100; ++i)); do
+    ./gen $i > input_file
     ./a < input_file > myAnswer
     ./brute < input_file > correctAnswer
-    diff -Z myAnswer correctAnswer > /dev/null || break
+    if ! diff -Z myAnswer correctAnswer > /dev/null; then
+        ok=0
+        break
+    fi
     echo "Passed test: "  $i
 done
-echo "WA on the following test:"
-cat input_file
-echo "Your answer is:"
-cat myAnswer
-echo "Correct answer is:"
-cat correctAnswer
+
+if [ $ok -eq 0 ]; then
+	echo "WA on the following test:"
+	cat input_file
+	echo "Your answer is:"
+	cat myAnswer
+	echo "Correct answer is:"
+	cat correctAnswer
+fi
 
 echo "finished"
